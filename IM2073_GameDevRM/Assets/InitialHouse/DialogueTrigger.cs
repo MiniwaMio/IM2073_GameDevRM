f415bc0f5dac53cf26148e351af4554a7adc81d4
ramj0002 <rj_26@live.com>
2024-02-04 20:01:15 +0800

    terrain

---
+++ IM2073_GameDevRM/Assets/InitialHouse/DialogueTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private List<dialogueString> dialogueStrings = new List<dialogueString>();
+    [SerializeField] private Transform NPCTransform;
+
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !hasSpoken)
+        {
+            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, NPCTransform);
+            hasSpoken = true;
+        }
+    }
+}
+
+[System.Serializable]
+
+public class dialogueString
+{
+    public string text; //represent the text that the npc says
+    public bool isEnd; 
+
+    [Header("Branch")]
+    public bool isQuestion;
+    public string answwerOption1;
+    public int option1IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+}
